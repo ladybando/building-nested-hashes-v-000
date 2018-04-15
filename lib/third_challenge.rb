0a3283f459d0d4befae93ae7bea395c77776e296
Ayana Bando <ayanaflax@yahoo.com>
2018-04-15 22:20:32 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,15 +2,16 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name =>"Lord Mantague",
+        :name =>"Lord Montague",
         :age => "53"
         },
       :matriarch => {
-        :name =>"Lady Mantague"
+        :name =>"Lady Montague"
         :age => "54"
         },
       :hero => {
-
+        :name => "Romeo"
+        :age => "15"
         },
       :hero_friends => []
    },
